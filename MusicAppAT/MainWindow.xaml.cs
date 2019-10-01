7be732f19e1b8651f6b3c51cf125983508b2487b
Corey Jensen <Corey.Jensen@southeasttech.edu>
2019-10-01 12:10:12 -0500

    Created the UI

--- MusicAppAT/MainWindow.xaml.cs
+++ MusicAppAT/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Runtime.InteropServices;
 
 namespace MusicAppAT
 {
@@ -23,6 +24,34 @@ namespace MusicAppAT
         public MainWindow()
         {
             InitializeComponent();
+            Init();
         }
+
+        public void Init()
+        {
+
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            //Console.WriteLine(GetMousePosition());
+        }
+        /*
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+        */
     }
 }
